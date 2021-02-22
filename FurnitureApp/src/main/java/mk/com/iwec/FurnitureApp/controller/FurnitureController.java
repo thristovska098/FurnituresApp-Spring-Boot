5e8b07e6d4b4f17b091bdc372e51ff74e6bd6a17
thristovska098 <thristovska098@gmail.com>
2021-02-22 20:26:47 +0100

    Added Request Mapping

--- FurnitureApp/src/main/java/mk/com/iwec/FurnitureApp/controller/FurnitureController.java
+++ FurnitureApp/src/main/java/mk/com/iwec/FurnitureApp/controller/FurnitureController.java
@@ -3,11 +3,13 @@ package mk.com.iwec.FurnitureApp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import mk.com.iwec.FurnitureApp.dto.FurnituresDto;
 import mk.com.iwec.FurnitureApp.service.FurnitureService;
 
 @RestController
+@RequestMapping("/api")
 public class FurnitureController implements Controller<FurnituresDto> {
 
 	@Autowired
